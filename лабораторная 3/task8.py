ca95906b6926e522f06d3b45113e9f9ec6d7be2f
NeKitos <115574457+NeKitos@users.noreply.github.com>
2022-10-17 20:11:35 +0300

    Update task8.py

--- лабораторная 3/task8.py
+++ лабораторная 3/task8.py
@@ -7,10 +7,9 @@ month = 0  # количество месяцев, которое можно пр
 
 # TODO Оформить решение
 loss = salary - (spend + (spend * (month * increase)))
-ostatok = money_capital + loss
 
-while ostatok >= 0:
-    ostatok = money_capital + loss
+
+while money_capital >= (spend + (spend * ((month + 1) * increase))):
     month += 1
     loss = salary - (spend + (spend * (month * increase)))
     money_capital += loss
