f6ebff142cf5b2e83259e5d2c9d7b88eacddaeff
NeKitos <115574457+NeKitos@users.noreply.github.com>
2022-11-14 18:07:06 +0300

    Update task3.py

--- Лабораторная работа 5/task3.py
+++ Лабораторная работа 5/task3.py
@@ -1,10 +1,10 @@
 import random
 
 
-def get_unique_list_numbers() -> list[int]:
+def get_unique_list_numbers(start=-10, end=10) -> list[int]:
     list_ = []
     while len(set(list_)) != 15:
-        random_number = random.randint(-10, 10)
+        random_number = random.randint(start, end)
         if random_number not in list_:
             list_.append(random_number)
     return list_
