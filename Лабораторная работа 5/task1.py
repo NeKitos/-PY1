54e88d0efcd97a23f1eef2c428fadde4f930da18
NeKitos <115574457+NeKitos@users.noreply.github.com>
2022-11-14 18:06:21 +0300

    Update task1.py

--- Лабораторная работа 5/task1.py
+++ Лабораторная работа 5/task1.py
@@ -1,5 +1,4 @@
-# TODO решить с помощью list comprehension и распечатать его
-answer = [{'bin': bin(number), 'dec': number, 'hex': hex(number), 'oct': oct(number)} for number in range(16)]
 from pprint import pprint
+answer = [{'bin': bin(number), 'dec': number, 'hex': hex(number), 'oct': oct(number)} for number in range(16)]
 
 pprint(answer)
