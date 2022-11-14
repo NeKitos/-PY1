d62fe1f7408d73a4c4955fbcd3880b82ed49efc6
NeKitos <115574457+NeKitos@users.noreply.github.com>
2022-11-14 18:07:58 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -2,12 +2,13 @@ import random
 import string
 
 
-def get_random_password() -> str:
+def get_random_password(how_long=8) -> str:
     lettersh = string.ascii_uppercase
     lettersl = string.ascii_lowercase
     nombers = string.digits
     list_simbols = lettersl + lettersh + nombers
-    password = random.sample(list_simbols, 8)
+    password = random.sample(list_simbols, how_long)
     return ''.join(password)
+
 
 print(get_random_password())
